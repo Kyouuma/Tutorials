bfe9fd6f136515b1b18f27ace35bff9b74ac8f02
ohafsi <oussamahafsi.official@gmail.com>
2020-03-24 21:43:36 +0100

    FULL CI

---
+++ quarkus-native-development/src/test/java/io/kyoumatrix/NativeSimpleRestEndpointIT.java
@@ -0,0 +1,9 @@
+package io.kyoumatrix;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeSimpleRestEndpointIT extends SimpleRestEndpointTest {
+
+    // Execute the same tests but in native mode.
+}
